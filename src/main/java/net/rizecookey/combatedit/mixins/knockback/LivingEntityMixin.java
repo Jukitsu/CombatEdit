35adb0f16e7f77db1163be7ffdc2ea876bdd2bdb
Jukitsu <ninjax2312@gmail.com>
2024-05-11 14:58:28 +0200

    Kohi knockback lol

--- src/main/java/net/rizecookey/combatedit/mixins/knockback/LivingEntityMixin.java
+++ src/main/java/net/rizecookey/combatedit/mixins/knockback/LivingEntityMixin.java
@@ -1,28 +1,44 @@
 package net.rizecookey.combatedit.mixins.knockback;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import net.rizecookey.combatedit.utils.KnockbackOptions;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin {
-    @Unique private Vec3d cache_vec3d;
-    @Unique private double cache_f;
-    @Inject(method = "takeKnockback", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;setVelocity(DDD)V", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILSOFT)
-    public void loadVec3d(double f, double d, double e, CallbackInfo ci, Vec3d vec3d, Vec3d vec3d2) {
-        this.cache_f = f;
-        this.cache_vec3d = vec3d;
+public abstract class LivingEntityMixin extends Entity {
+
+    protected LivingEntityMixin(EntityType<? extends LivingEntity> entityType, World level) {
+        super(entityType, level);
     }
 
-    @ModifyArg(method = "takeKnockback", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;setVelocity(DDD)V"), index = 1)
-    public double changeKnockbackY(double y) {
-        y = cache_vec3d.y / 2.0 + cache_f;
-        return cache_vec3d.y > 0.4D ? 0.4D : y;
+
+    @Shadow public abstract double getAttributeValue(EntityAttribute attribute);
+
+
+    @Inject(method="takeKnockback", at=@At("HEAD"), cancellable = true)
+    public void handleTakeKnockback(double d, double x, double z, CallbackInfo ci) {
+        double resistanceFactor = 1.0D - this.getAttributeValue(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE);
+        if (resistanceFactor > 0.0D) {
+            this.velocityDirty = true;
+            Vec3d velocity = this.getVelocity();
+            Vec3d knockbackVector = (new Vec3d(x, 0.0D, z)).normalize().multiply(KnockbackOptions.knockbackHorizontal * resistanceFactor);
+            this.setVelocity(
+                    velocity.x / KnockbackOptions.knockbackFriction - knockbackVector.x,
+                    Math.min(KnockbackOptions.knockbackVerticalLimit, velocity.y / 2.0D + KnockbackOptions.knockbackVertical * resistanceFactor),
+                    velocity.z / KnockbackOptions.knockbackFriction - knockbackVector.z
+            );
+        }
+        ci.cancel();
     }
+
 }
--- src/main/java/net/rizecookey/combatedit/mixins/knockback/PlayerEntityMixin.java
+++ src/main/java/net/rizecookey/combatedit/mixins/knockback/PlayerEntityMixin.java
@@ -3,6 +3,7 @@ package net.rizecookey.combatedit.mixins.knockback;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
+import net.rizecookey.combatedit.utils.KnockbackOptions;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -11,7 +12,9 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public abstract class PlayerEntityMixin {
     @Redirect(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;takeKnockback(DDD)V"))
     public void handleTakeKnockback(LivingEntity livingEntity, double speed, double xMovement, double zMovement) {
-        speed = (float) ((double) speed * (1.0D - livingEntity.getAttributeInstance(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE).getValue()));
-        livingEntity.addVelocity(-(xMovement * speed), 0.1D, -(zMovement * speed));
+        double amount = 2.0D * speed;
+        amount *= KnockbackOptions.knockbackExtraHorizontal;
+        amount *= 1.0D - livingEntity.getAttributeInstance(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE).getValue();
+        livingEntity.addVelocity(-(xMovement * amount), KnockbackOptions.knockbackExtraVertical, -(zMovement * amount));
     }
 }
